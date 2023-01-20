0742d06468b7d0d1f2376800f882157fb29d0b9f
garzonwilmer5 <garzonwilmer5@gmail.com>
2023-01-20 11:20:54 -0500

    [FEAT] use only .md README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pip._internal.req import parse_requirements
 
 from ros2_graph import __version__
 
-with open("README.rst") as readme:
+with open("README.md") as readme:
     long_description = readme.read()
 
 install_reqs = list(parse_requirements("requirements.txt", session=False))
@@ -19,6 +19,7 @@ setuptools.setup(
     url="https://github.com/kiwicampus/ros2_graph",
     description="Generate mermaid description of ROS2 graphs to add on your markdown files.",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=["ros2_graph"],
     install_requires=requirements,
     entry_points={"console_scripts": ["ros2_graph = ros2_graph:__main__.main"]},
