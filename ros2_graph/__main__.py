2ce3d1933408ff9501ecb89ab92a5458e862469a
Carlos Andrés Álvarez Restrepo <charlie_cha@outlook.com>
2023-01-20 10:04:28 -0500

    Add lincence python (#8)

--- ros2_graph/__main__.py
+++ ros2_graph/__main__.py
@@ -1,9 +1,23 @@
-import sys
+#! /usr/bin/env python3
+# Copyright 2023 Kiwicampus Inc.
+#
+# Licensed under GNU GENERAL PUBLIC, version 3 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
-from typing import List, Dict
 from functools import reduce
 
 from .graph_generator import get_node_graph
+
 
 def main(args):
     nodes = args.nodes
