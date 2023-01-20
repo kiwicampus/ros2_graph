a962eeb7117452941da84f17eada6ddc121dedfb
Wilmer Garzón <71189684+garzonwilmer5@users.noreply.github.com>
2023-01-20 09:25:23 -0500

    [FIX] repo url on setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except:
 setuptools.setup(
     name="ros2_graph",
     version=__version__,
-    url="https://github.com/kiwicampus/ros2_graphs_mermaid",
+    url="https://github.com/kiwicampus/ros2_graph",
     description="Generate mermaid description of ROS2 graphs to add on your markdown files.",
     long_description=long_description,
     packages=["ros2_graph"],
